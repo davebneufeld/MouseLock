49b527d22d84f566b4be9fc205675a9a91fb7e8a
Mouaijin <thechristopheriandavis@gmail.com>
2015-01-31 00:17:21 -0500

    Basic functionality introduced
    Program now handles (most) internal and external mouse movement events before they are processed.
    
    Local hotkey (ctrl+alt+shift+L) toggles

--- MouseLock/MouseLockForm.Designer.cs
+++ MouseLock/MouseLockForm.Designer.cs
@@ -28,20 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.lockToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label = new System.Windows.Forms.Label();
+            this.eventProvider = new MouseKeyboardActivityMonitor.Controls.MouseKeyEventProvider();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lockToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(234, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // lockToolStripMenuItem
+            // 
+            this.lockToolStripMenuItem.CheckOnClick = true;
+            this.lockToolStripMenuItem.Name = "lockToolStripMenuItem";
+            this.lockToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Alt) 
+            | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.L)));
+            this.lockToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.lockToolStripMenuItem.Text = "Lock";
+            this.lockToolStripMenuItem.Click += new System.EventHandler(this.lockToolStripMenuItem_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(73, 57);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(53, 13);
+            this.label.TabIndex = 2;
+            this.label.Text = "Unlocked";
+            // 
+            // eventProvider
+            // 
+            this.eventProvider.Enabled = false;
+            this.eventProvider.HookType = MouseKeyboardActivityMonitor.Controls.HookType.Global;
+            this.eventProvider.MouseMoveExt += new System.EventHandler<MouseKeyboardActivityMonitor.MouseEventExtArgs>(this.event_MouseMoveExt);
             // 
             // MouseLockForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.ClientSize = new System.Drawing.Size(234, 116);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "MouseLockForm";
             this.Text = "MouseLock";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem lockToolStripMenuItem;
+        private System.Windows.Forms.Label label;
+        private MouseKeyboardActivityMonitor.Controls.MouseKeyEventProvider eventProvider;
     }
 }
 
--- MouseLock/MouseLockForm.cs
+++ MouseLock/MouseLockForm.cs
@@ -1,20 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace MouseLock
 {
     public partial class MouseLockForm : Form
     {
+
         public MouseLockForm()
         {
             InitializeComponent();
+            eventProvider.Enabled = false;
+            label.Text = "Unlocked";
         }
+
+        private void lockToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(eventProvider.Enabled)
+                Unlock();
+            else
+            {
+                Lock();
+            }
+        }
+
+        private void Lock()
+        {
+            eventProvider.Enabled = true;
+            label.Text = "Locked";
+
+        }
+
+        private void Unlock()
+        {
+            eventProvider.Enabled = false;
+            label.Text = "Unlocked";
+        }
+
+        private void event_MouseMoveExt(object sender, MouseKeyboardActivityMonitor.MouseEventExtArgs e)
+        {
+            e.Handled = true;
+        }
+
+
     }
 }
